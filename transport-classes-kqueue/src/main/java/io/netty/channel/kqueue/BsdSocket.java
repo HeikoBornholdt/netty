5026cde9f5aa2df8964a6dd672dfa8f1ec089f03
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-10-21 11:48:23 +0200

    epoll prep

--- transport-classes-kqueue/src/main/java/io/netty/channel/kqueue/BsdSocket.java
+++ transport-classes-kqueue/src/main/java/io/netty/channel/kqueue/BsdSocket.java
@@ -41,7 +41,8 @@ import static io.netty.util.internal.ObjectUtil.checkNotNull;
  */
 final class BsdSocket extends Socket {
     private static final String TUN_DEVICE_PREFIX = "utun";
-    private static final IllegalArgumentException TUN_ILLEGAL_NAME_EXCEPTION = new IllegalArgumentException("Device name must be 'utun<index>' or null.");
+    private static final IllegalArgumentException TUN_ILLEGAL_NAME_EXCEPTION =
+            new IllegalArgumentException("Device name must be 'utun<index>' or null.");
 
     // These limits are just based on observations. I couldn't find anything in header files which formally
     // define these limits.
@@ -282,16 +283,13 @@ final class BsdSocket extends Socket {
                 if (addr.ifName().startsWith(TUN_DEVICE_PREFIX)) {
                     try {
                         index = Integer.parseInt(addr.ifName().substring(TUN_DEVICE_PREFIX.length()));
-                    }
-                    catch (final NumberFormatException e) {
+                    } catch (final NumberFormatException e) {
                         throw TUN_ILLEGAL_NAME_EXCEPTION;
                     }
-                }
-                else {
+                } else {
                     throw TUN_ILLEGAL_NAME_EXCEPTION;
                 }
-            }
-            else {
+            } else {
                 index = 0;
             }
             int res = bindTun(intValue(), index);
@@ -303,11 +301,11 @@ final class BsdSocket extends Socket {
         }
     }
 
-    public static native int bindTun(final int fd, final int index);
+    public static native int bindTun(int fd, int index);
 
     public SocketAddress localAddressTun() {
         return new TunAddress(localAddressTun(intValue()));
     }
 
-    public static native String localAddressTun(final int fd);
+    public static native String localAddressTun(int fd);
 }
--- transport-classes-kqueue/src/main/java/io/netty/channel/kqueue/TunAddress.java
+++ transport-classes-kqueue/src/main/java/io/netty/channel/kqueue/TunAddress.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.channel.kqueue;
 
 import java.net.SocketAddress;
@@ -12,7 +27,7 @@ public class TunAddress extends SocketAddress {
     private static final long serialVersionUID = -584786182484350484L; // NOSONAR
     private final String ifName;
 
-    public TunAddress(final String ifName) {
+    public TunAddress(String ifName) {
         this.ifName = ifName;
     }
 
@@ -33,8 +48,7 @@ public class TunAddress extends SocketAddress {
     public String toString() {
         if (ifName == null) {
             return "";
-        }
-        else {
+        } else {
             return ifName;
         }
     }
